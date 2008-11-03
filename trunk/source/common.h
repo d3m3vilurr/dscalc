d0d8529a436bdbf96789ac619412ca4b6623d742
d3m3vilurr <d3m3vilurr@267056ce-a998-11dd-a77a-2d921c902ce3>
2008-11-03 11:31:37 +0000

    first commit. ds calc project start

---
+++ trunk/source/common.h
@@ -0,0 +1,33 @@
+#ifndef __COMMON_H__
+#define __COMMON_H__
+
+#include <PA9.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define UP_LCD 1
+#define DOWN_LCD 0
+
+#define MAXCOL 32
+#define MAXROW 24
+
+float calc(const char*);
+//void plot(const char*, float, float);
+
+float add(float, float);
+float sub(float, float);
+float mul(float, float);
+float div(float, float);
+
+#define PI 3.1415926535897931
+#define E  2.7182818284590451
+
+float rad2deg(float);
+float deg2rad(float);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
