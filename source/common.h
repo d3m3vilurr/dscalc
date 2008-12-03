9eebd56e9325562d3ba374f172a1dc06d2726750
d3m3vilurr <d3m3vilurr@267056ce-a998-11dd-a77a-2d921c902ce3>
2008-12-03 23:22:37 +0000

    check error

--- source/common.h
+++ source/common.h
@@ -13,18 +13,25 @@ Num eval2(char*, Num x);
 
 //void plot(const char*, float, float);
 
+/*
 float add(float, float);
 float sub(float, float);
 float mul(float, float);
 float div(float, float);
+*/
+//#define PI 3.1415926535897931
+//#define E  2.7182818284590451
 
-#define PI 3.1415926535897931
-#define E  2.7182818284590451
-
+#define PI 3.14159f
+#define E  2.71828f
+/*
 float rad2deg(float);
 float deg2rad(float);
-
+*/
+/*
 int randInt(int);
+*/
+
 #ifdef __cplusplus
 }
 #endif
